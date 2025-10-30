10d7ae7dd493150306ec01425e546bd5911fd759
che250008036 <che250008036@iiti.ac.in>
2025-10-30 16:38:11 +0530

    Add dynamic sales array with resizing functionality
    Implement dynamic array for sales data and resize it.

---
+++ question1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int* sales = new int[100];
+    for (int i = 0; i < 100; ++i) {
+        sales[i] = i + 1;  
+    }
+    cout << "Sales data before resizing :" << endl;
+    for (int i = 0; i < 100; ++i) {
+        cout << sales[i] << " ";
+    }
+    cout << endl;
+    int* resized_sales = new int[200];
+    for (int i = 0; i < 100; ++i) {
+        resized_sales[i] = sales[i];
+    }
+    for (int i = 100; i < 200; ++i) {
+        resized_sales[i] = i + 1;  
+    }
+    
+    cout << "Sales data after resizing :" << endl;
+    for (int i = 100; i < 200; ++i) {
+        cout << resized_sales[i] << " ";
+    }
+    cout << endl;
+    delete[] sales;        
+    delete[] resized_sales;
+    return 0;
+}
+
+
+	
